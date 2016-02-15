9c8df06f287d32401b2b51c546d8e55316ee68a7
Benjamin Connick (RIT Student) <bxc3201@ad.rit.edu>
2016-02-15 14:52:20 -0500

    added code to Rotate.cs

--- GrowingUp/Assets/Scripts/Rotate.cs
+++ GrowingUp/Assets/Scripts/Rotate.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Rotate : MonoBehaviour {
 
+    public float RotationSpeed;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,6 @@ public class Rotate : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
 	}
 }
