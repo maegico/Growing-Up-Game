f3d4e64635fdcb74118f7ace17ecf41f1a4cb356
Thomas Landi (RIT Student) <txl3829@ad.rit.edu>
2016-02-21 16:20:43 -0500

    Added player movement

---
+++ GrowingUp/Assets/Scripts/GameManagerInit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerInit : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GrowingUp/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    Location.lane currentLane;
+
+    
+    
+	// Use this for initialization
+	void Start () {
+        currentLane = Location.lane.middle;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) MoveLeft();
+        if (Input.GetKeyDown(KeyCode.RightArrow)) MoveRight();
+    }
+
+    void MoveRight()
+    {
+        switch  (currentLane)
+        {
+            case Location.lane.left:
+                currentLane = Location.lane.middle;
+                gameObject.transform.Translate(new Vector3(9, 0, 0));
+                break;
+            case Location.lane.middle:
+                currentLane = Location.lane.right;
+                gameObject.transform.Translate(new Vector3(9, 0, 0));
+                break;
+            case Location.lane.right:
+                break;
+            default:
+                break;
+        }
+    }
+
+    void MoveLeft()
+    {
+        switch (currentLane)
+        {
+            case Location.lane.left:
+                break;
+            case Location.lane.middle:
+                currentLane = Location.lane.left;
+                gameObject.transform.Translate(new Vector3(-9, 0, 0));
+                break;
+            case Location.lane.right:
+                currentLane = Location.lane.middle;
+                gameObject.transform.Translate(new Vector3(-9, 0, 0));
+                break;
+            default:
+                break;
+        }
+
+    }
+}
